945e371ea61268a03fb121cc05c9b0f51b3c90ae
Stéphane ARCELLIER <36193013+stepharc@users.noreply.github.com>
2018-04-21 11:27:10 +0200

    [21-04] Tampon v4
    - La couleur du tampon est désormais choisie aléatoirement au démarrage de la scène.
    - La communication se fait désormais correctement entre les scripts du tampon et de la surface de jeu : cette dernière connaît désormais la couleur du tampon pour générer les traces de la bonne couleur, l'orientation du tampon, sa dimension X et Z et le nom de l'objet représentant le tampon.
    - Il n'est désormais plus possible de modifier l'orientation du tampon lorsque ce dernier est en plein animation de saut.
    - Lorsque le tampon touche la surface de jeu, une trace est laissée sur la surface de jeu : elle est de même dimension X et Z, de même couleur et de même orientation que le tampon lorsque le saut a été initié.
    
    A faire : limiter la zone jouable à la surface de jeu uniquement.

--- Assets/Mini-Games/Tampon/Scripts/MG_Tampon.cs
+++ Assets/Mini-Games/Tampon/Scripts/MG_Tampon.cs
@@ -6,25 +6,36 @@ public class MG_Tampon : MonoBehaviour {
     private List<Joycon> joycons;
     private Joycon j;
     private Vector3 gyro;
+    private int jc_ind = 0;
     private MG_Tampon_Surface sInstance;
     private bool jumpMode;
-
-    private int jc_ind = 0;
+    private Color color;
 
     IEnumerator jumpAnim()
     {
-        float waitTime = .4F;
+        float waitTime = .4F, jumpHeight = 0.5f;
         //Tampon se lève
-        gameObject.transform.localPosition += new Vector3(0, 0.5f, 0);
+        gameObject.transform.localPosition += new Vector3(0, jumpHeight, 0);
         yield return new WaitForSeconds(waitTime);
         //On fait avancer le tampon
         gameObject.transform.Translate(Vector3.forward);
         yield return new WaitForSeconds(waitTime);
         //On le repose
-        gameObject.transform.localPosition -= new Vector3(0, 0.5f, 0);
+        gameObject.transform.localPosition -= new Vector3(0, jumpHeight, 0);
         yield return new WaitForSeconds(.2F);
-        //On n'oublie pas d'autoriser à nouveau le mouvement du tampon vu que celle en cours est terminée.
+        //On n'oublie pas d'autoriser à nouveau le mouvement du tampon vu que celle en cours vient de se terminer.
         jumpMode = false;
+    }
+
+    //Choisit une couleur au hasard et l'applique au tampon.
+    private void setRandomizedColor()
+    {
+        float r, g, b;
+        r = Random.Range(0.0f, 1.0f);
+        g = Random.Range(0.0f, 1.0f);
+        b = Random.Range(0.0f, 1.0f);
+        color = new Color(r, g, b, 1.0f);
+        gameObject.GetComponent<Renderer>().material.color = color;
     }
 
     // Use this for initialization
@@ -37,8 +48,13 @@ public class MG_Tampon : MonoBehaviour {
             gyro = j.GetGyro();
             jumpMode = false;
             //Permet la communication entre le tampon et la surface de jeu.
-            sInstance = GetComponent<MG_Tampon_Surface>();
-            gameObject.GetComponent<Renderer>().material.color = Color.red;
+            sInstance = GameObject.Find("Surface").GetComponent<MG_Tampon_Surface>();
+            setRandomizedColor();
+            //Envoi des données importantes à la surface de jeu (Orientation, dimensions, couleur, nom de l'objet)
+            sInstance.setTamponRotation(gameObject.transform.rotation.eulerAngles.y);
+            sInstance.setTamponScales(gameObject.transform.localScale.x, gameObject.transform.localScale.z);
+            sInstance.setTamponName(gameObject.name);
+            sInstance.setTrackColor(color);
         }
         else
         {
@@ -51,12 +67,16 @@ public class MG_Tampon : MonoBehaviour {
     {
         //Modifie l'orientation du tampon (Axe Y) selon l'axe Z du gyroscope
         float orientation = j.GetGyro().z;
-        gameObject.transform.Rotate(new Vector3(0, orientation, 0));
+        if (!jumpMode)
+        {
+            gameObject.transform.Rotate(new Vector3(0, orientation, 0));
+        }
         //Si le bouton "Flèche vers le haut" est pressé et que le tampon n'est pas en plein animation, on fait bouger le tampon.
         if ((j.GetButtonDown(Joycon.Button.DPAD_UP)) && !jumpMode)
         {
             jumpMode = true;
             Debug.Log("JUMP !");
+            sInstance.setTamponRotation(gameObject.transform.rotation.eulerAngles.y);
             //Pour que l'animation de mouvement ne se fasse pas instantanément, on utilise
             //une fonction de co-routine jumpAnim et on l'appelle avec StartCoroutine.
             StartCoroutine(jumpAnim());
--- Assets/Mini-Games/Tampon/Scripts/MG_Tampon_Surface.cs
+++ Assets/Mini-Games/Tampon/Scripts/MG_Tampon_Surface.cs
@@ -3,10 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MG_Tampon_Surface : MonoBehaviour {
+    private Color tColor;
+    private float tRotateY, tScaleX, tScaleZ;
+    private string tName;
+
+    //Récupère la couleur du tampon pour que la couleur des traces qu'il laisse soit identique avec le tampon.
+    public void setTrackColor(Color c)
+    {
+        tColor = c;
+    }
+
+    //Récupère l'axe de rotation Y du tampon.
+    public void setTamponRotation(float f)
+    {
+        tRotateY = f;
+    }
+
+    //Récupère les dimensions X et Z du tampon.
+    public void setTamponScales(float fx, float fz)
+    {
+        tScaleX = fx;
+        tScaleZ = fz;
+    }
+
+    //Récupère le nom de l'objet représentant le tampon.
+    public void setTamponName(string s)
+    {
+        tName = s;
+    }
+
+    //Affiche la marque laissée par le tampon lors de son déplacement sur la surface de jeu.
+    private void drawTrack(Collision collision)
+    {
+        //Coordonnée y de la face supérieure (sur laquelle se trouve le tampon) de la surface de jeu.
+        float ySurface = GetComponent<Renderer>().bounds.max.y;
+        GameObject track = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //On place la trace de telle sorte qu'elle couvre toute la zone de collision
+        track.transform.position = collision.collider.bounds.center;
+        //La coordonnée y est changée pour que la trace se trouve pile au dessus de la surface de jeu.
+        track.transform.position = new Vector3(track.transform.position.x, ySurface, track.transform.position.z);
+        //On oriente la trace de telle sorte qu'elle corresponde à celle du tampon à la fin de son saut.
+        track.transform.Rotate(new Vector3(0, tRotateY, 0));
+        //On désactive les collisions de la trace pour éviter que le tampon ne se trouve au dessus de celle-ci, évitant ainsi la
+        //rupture de contact entre le tampon et la surface de jeu.
+        track.GetComponent<Collider>().enabled = false;
+        track.GetComponent<Renderer>().material.color = tColor;
+        track.transform.localScale = new Vector3(tScaleX, 0.025f, tScaleZ);
+        //La trace créée ne sera pas visible dans l'onglet Hiérarchie sur Unity3D.
+        track.hideFlags = HideFlags.HideInHierarchy;
+    }
 
     // Use this for initialization
     void Start () {
-
+        
     }
 	
 	// Update is called once per frame
@@ -14,19 +63,22 @@ public class MG_Tampon_Surface : MonoBehaviour {
 		
 	}
 
-    //Se déclenche quand le tampon est en contact avec la surface de jeu.
+    //Exécute le code de cette fonction quand le tampon est en contact avec la surface de jeu.
     //Note : Pour assurer le bon fonctionnement de cette méthode, nous avons été obligés d'ajouter des composants
     //Rigidbody aux objets Tampon et Surface, même si nous n'utilisons pas les propriétés physiques fournies.
     private void OnCollisionEnter(Collision collision)
     {
-        ContactPoint cp;
-        Vector3[] points = new Vector3[4];
-        for(int i = 0; i < collision.contacts.Length; i++)
+        if (collision.gameObject.name == tName)
         {
-            cp = collision.contacts[i];
-            points[i] = cp.point;
-            //Affiche le point de collision en noir sur la vue Scene.
-            Debug.DrawRay(cp.point, cp.normal, Color.black, int.MaxValue, false);
+            ContactPoint cp;
+            for (int i = 0; i < collision.contacts.Length; i++)
+            {
+                cp = collision.contacts[i];
+                Debug.Log("Point " + i + " : " + cp.point);
+                //Affiche le point de collision en noir sur la vue Editeur.
+                Debug.DrawRay(cp.point, cp.normal, Color.black, int.MaxValue, false);
+            }
+            drawTrack(collision);
         }
     }
 }
