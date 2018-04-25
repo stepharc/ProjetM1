4d309489e198976d177f95ec22a0387d43d92ed0
goldsamu <36366367+goldsamu@users.noreply.github.com>
2018-04-25 12:36:27 +0200

    Add files via upload

---
+++ Assets/Mini-Games/Libre/Scripts/Observation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Observation : MonoBehaviour
+{
+    private List<Joycon> joycons;
+    private Joycon j;
+    public int jc_ind = 0;
+
+    void Start()
+    {
+        joycons = JoyconManager.Instance.j;
+        if (joycons.Count > 0)
+        {
+            j = joycons[jc_ind];
+        }
+    }
+
+    void Update()
+    {
+        if (joycons.Count > 0)
+        {
+                // Rotation grâce au gyroscope.
+                gameObject.transform.rotation = j.GetVector();
+        }
+    }
+}
