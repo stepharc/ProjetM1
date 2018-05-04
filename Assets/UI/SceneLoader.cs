da49342685a8179ce8b26f42b191224dd0d7bd74
goldsamu <36366367+goldsamu@users.noreply.github.com>
2018-05-04 20:22:21 +0200

    Add files via upload

---
+++ Assets/UI/SceneLoader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void SceneLoad(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
