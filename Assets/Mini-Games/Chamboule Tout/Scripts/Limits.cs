e1bcbc223ae9faedbd28eb56ef71485a9c33ce11
goldsamu <36366367+goldsamu@users.noreply.github.com>
2018-05-04 20:27:42 +0200

    Add files via upload

---
+++ Assets/Mini-Games/Chamboule Tout/Scripts/Limits.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Limits : MonoBehaviour
+{
+    /* Un objet est détruit quand il sort des limites du jeu. */
+    void OnCollisionEnter(Collision other)
+    {
+        Destroy(other.gameObject);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
