af2d0e1eb8b9dbd65c15b940f5170ce81074d4ee
goldsamu <36366367+goldsamu@users.noreply.github.com>
2018-05-04 20:30:13 +0200

    Add files via upload

--- Assets/Mini-Games/Labyrinthe/Scripts/CameraScript.cs
+++ Assets/Mini-Games/Labyrinthe/Scripts/CameraScript.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 public class CameraScript : MonoBehaviour
 {
-    public GameObject ball;
-    public Vector3 offset;
+    public GameObject focus; // L'objet qui a le focus.
+    public Vector3 offset; // La distance entre la caméra et l'objet qui a le focus.
 
     /* Déplace la caméra en fonction de l'objet ball avec un offset particulier */
     void Update()
     {
-        transform.position = new Vector3(ball.transform.position.x + offset.x, ball.transform.position.y + offset.y, ball.transform.position.z + offset.z);
+        transform.position = new Vector3(focus.transform.position.x + offset.x, focus.transform.position.y + offset.y, focus.transform.position.z + offset.z);
     }
 }
