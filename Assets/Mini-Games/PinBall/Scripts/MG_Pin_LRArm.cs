c944f50e7043cd730ac17da617b9041992225f76
Stéphane ARCELLIER <36193013+stepharc@users.noreply.github.com>
2018-05-08 13:38:44 +0200

    [08-05] Pinball v2
    Implémentation du mouvement des bras du mini-jeu Pinball :
    
    - Tant que le bouton ZL (Joycon Gauche) est enfoncé, rotation du bras gauche vers le haut, jusqu'à une certaine limite.
    - Bouton ZL relâché : retour du bras gauche vers sa rotation d'origine.
    
    Idem pour le bras droit (sauf pour le bouton d’interaction qui est ZR (Joycon Droit)).
    
    Ces actions peuvent être annulées en cours d'exécution en relâchant le bouton pendant la rotation vers le haut ou en appuyant sur le bouton pendant le retour du bras vers sa position d'origine.
    
    Demain : implémentation du mouvement du support de bille, afin de pouvoir projeter la bille vers la surface de jeu.

---
+++ Assets/Mini-Games/PinBall/Scripts/MG_Pin_LRArm.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG_Pin_LRArm : MonoBehaviour {
+    private float baseYRotation;
+
+    public float getBaseRotation()
+    {
+        return baseYRotation;
+    }
+
+	// Use this for initialization
+	void Start () {
+        MG_Pin_Arms father = gameObject.transform.parent.GetComponent<MG_Pin_Arms>();
+        baseYRotation = gameObject.transform.rotation.eulerAngles.y;
+		if(gameObject.name.CompareTo("LeftArm") == 0)
+        {
+            father.setLeftArm(gameObject.transform);
+        }
+        else
+        {
+            if (gameObject.name.CompareTo("RightArm") == 0)
+            {
+                father.setRightArm(gameObject.transform);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
