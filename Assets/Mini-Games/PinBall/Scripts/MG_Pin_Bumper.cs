5f9a17f84ef814037ffa5f952c6b89ec312006ab
Stéphane ARCELLIER <36193013+stepharc@users.noreply.github.com>
2018-05-10 12:04:28 +0200

    [10-05] Pinball v4
    Implémentation du comportement des bumpers : lorsque la bille touche un bumper, la bille est projetée en arrière.
    
    Amélioration de l'intéraction entre la bille et les bras du Pinball : la bille est projetée avec une force variable selon la distance entre la bille et l'extremité du bras : plus elle est grande, plus cette force de projection diminue. Ce mouvement se réalise si au préalable la bille est sur le bras puis qu'il y a mouvement du dit bras.
    
    Modification de la forme de certains murs de la surface de jeu pour éviter que la bille ne reste bloquée sur une position.
    
    Demain : implémentation des téléporteurs.

---
+++ Assets/Mini-Games/PinBall/Scripts/MG_Pin_Bumper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG_Pin_Bumper : MonoBehaviour {
+    private Color baseColor;
+    private float pushStrength;
+
+    IEnumerator changeColor()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+        yield return new WaitForSeconds(1);
+        gameObject.GetComponent<Renderer>().material.color = baseColor;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name.CompareTo("Bille") == 0)
+        {
+            Rigidbody r = collision.gameObject.GetComponent<Rigidbody>();
+            StartCoroutine("changeColor");
+            //Repousse la bille en arrière avec une certaine force.
+            Vector3 forceVec = -r.velocity.normalized * pushStrength;
+            //Ajouter ce mode permet d'ignorer les masses des deux objets en collision.
+            r.AddForce(forceVec, ForceMode.Acceleration);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        baseColor = gameObject.GetComponent<Renderer>().material.color;
+        pushStrength = 500f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Mini-Games/PinBall/Scripts/MG_Pin_LRArm.cs
+++ Assets/Mini-Games/PinBall/Scripts/MG_Pin_LRArm.cs
@@ -3,26 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MG_Pin_LRArm : MonoBehaviour {
-    private float baseYRotation;
+    private float baseYRotation, boundX, lastContactX, pushForce, basePushForce;
+    private Vector3 lastVelocity;
 
     public float getBaseRotation()
     {
         return baseYRotation;
     }
 
-	// Use this for initialization
-	void Start () {
+    private void OnCollisionStay(Collision collision)
+    {
+        //On garde en mémoire le dernier point de contact et la dernière vélocité de la bille.
+        if(collision.gameObject.name.CompareTo("Bille") == 0)
+        {
+            lastContactX = collision.contacts[0].point.x;
+            lastVelocity = collision.gameObject.GetComponent<Rigidbody>().velocity.normalized;
+        }
+    }
+
+    //La bille ne touche plus le bras, la bille est donc projetée !
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name.CompareTo("Bille") == 0)
+        {
+            Rigidbody r = collision.gameObject.GetComponent<Rigidbody>();
+            float distance;
+            //On calcule la distance entre le dernier point de contact et l'extremité du bras
+            if (boundX > lastContactX)
+            {
+                distance = boundX - lastContactX;
+            }
+            else
+            {
+                distance = lastContactX - boundX;
+            }
+            if(distance > 1)
+            {
+                //Plus la bille est loin de l'extremité du bras, plus la puissance de projection diminue.
+                pushForce = pushForce / distance;
+            }
+            Vector3 forceVec = lastVelocity * pushForce;
+            r.AddForce(forceVec, ForceMode.Acceleration);
+            if (pushForce != basePushForce) pushForce = basePushForce;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
         MG_Pin_Arms father = gameObject.transform.parent.GetComponent<MG_Pin_Arms>();
         baseYRotation = gameObject.transform.rotation.eulerAngles.y;
+        pushForce = basePushForce = 1000f;
 		if(gameObject.name.CompareTo("LeftArm") == 0)
         {
             father.setLeftArm(gameObject.transform);
+            boundX = gameObject.GetComponent<Renderer>().bounds.max.x;
         }
         else
         {
             if (gameObject.name.CompareTo("RightArm") == 0)
             {
                 father.setRightArm(gameObject.transform);
+                boundX = gameObject.GetComponent<Renderer>().bounds.min.x;
             }
         }
 	}
