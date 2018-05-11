42417d2dcfbfd1ef401d220eecaaadf22340e31c
Stéphane ARCELLIER <36193013+stepharc@users.noreply.github.com>
2018-05-11 13:08:56 +0200

    [11-05] Pinball final
    - Implémentation du comportement des téléporteurs : il y a téléportation de la bille si au moins deux téléporteurs sont connectés (dans ce mini-jeu : s'il ont la même couleur).
    
    Si la bille touche la boîte de collision d'un téléporteur sans connexion, la couleur de ce dernier est de nouveau choisi (afin d'espérer être connecté avec d'autres téléporteurs). Dans le cas où la bille entre dans un téléporteur avec plus d'une connexion, un téléporteur est sélectionné parmi ceux éligibles et devient la destination.
    
    Dans tous les cas suscités, utiliser un téléporteur les désactive tous pendant deux secondes.
    
    - Le support de bille est correctement replacé à sa position initiale une fois utilisé.
    
    - Correction d'un bug au niveau des bras du Pinball : si la bille quitte un bras à cause de la gravité, il n'y a plus de projection de la bille.
    
    - Ajout d'une zone déclencheur (Trigger) couvrant la zone se situant sous les bras du Pinball : si la bille touche ce trigger, elle est replacée à la position où elle était en début de partie.
    
    - Modifications apportées sur certains murs de la surface de jeu et sur la position des téléporteurs, afin d'éviter que la bille ne se retrouve bloquée sur une position.

---
+++ Assets/Mini-Games/PinBall/Scripts/MG_Pin_LZone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG_Pin_LZone : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name.CompareTo("Bille") == 0)
+        {
+            other.gameObject.GetComponent<MG_Pin_Bille>().setBackToSpawn();
+        }    
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Mini-Games/PinBall/Scripts/MG_Pin_OneTP.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG_Pin_OneTP : MonoBehaviour {
+    private MG_Pin_TPs father;
+
+    //Pendant un certain délai (2 secondes), les téléporteurs sont désactivés, afin d'éviter les téléportations successives.
+    IEnumerator disableTPs()
+    {
+        father.setWarpValue(true);
+        yield return new WaitForSeconds(2f);
+        father.setWarpValue(false);
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        bool b = father.getWarpValue();
+        //Si la bille entre en contact avec la bôîte de collision du téléporteur et que ces derniers ne sont pas désactivés.
+        if ((c.name.CompareTo("Bille") == 0) && (!b))
+        {
+            StartCoroutine("disableTPs");
+            Color[] cs = father.getColors();
+            int index = father.getIndexByColor(gameObject.GetComponent<Renderer>().material.color);
+            List<Transform> l = father.getAmputatedList(gameObject.transform, index);
+            if(l.Count != 0)
+            {
+                //On choisit un téléporteur au hasard parmi ceux éligibles.
+                int r = Random.Range(0, l.Count - 1);
+                Transform t = l[r];
+                //La bille est déplacée sous le téléporteur élu.
+                c.gameObject.transform.position = new Vector3(t.position.x, t.position.y - (c.gameObject.transform.lossyScale.y / 2), t.position.z);
+            }
+            //Le téléporteur sélectionne une nouvelle couleur aléatoirement parmi ceux proposés.
+            int rand = Random.Range(0, cs.Length);
+            gameObject.GetComponent<Renderer>().material.color = cs[rand];
+            father.setElementInList(gameObject.transform, rand);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        father = gameObject.transform.parent.GetComponent<MG_Pin_TPs>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
