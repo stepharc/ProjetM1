c9125745ac556647a40d70fe0e2a460a48cf2929
Stéphane ARCELLIER <36193013+stepharc@users.noreply.github.com>
2018-05-14 11:46:28 +0200

    [14-05] Jeu de la vie v1
    Ce mini-jeu sera une version simplifiée mais tout de même inspirée de ce jeu suivant : https://fr.wikipedia.org/wiki/Jeu_de_la_vie
    
    Ce mini-jeu a pour but de présenter la technologie GPU Instancing, rajoutée dans la dernière version d'Unity en date (https://docs.unity3d.com/Manual/GPUInstancing.html) : l'objectif est donc de montrer le gain de performances obtenu lorsque un grand nombre de gameObjects est représenté à l'écran.
    
    Le mini-jeu nécessite exactement un joycon droit et un autre gauche pour fonctionner.
    
    - Création de deux matériaux : WithGPUI (GPU Instancing ON) et WithoutGPUI (GPU Instancing OFF), chemin : Resources/Materials/<fichier matériau>
    
    - L'utilisateur peut contrôler le mouvement du cube qu'il contrôle via les sticks des Joycons. (Gauche : Avant / Arrière; Droit : Haut / Bas / Gauche / Droite)
    
    - Matériau par défaut du cube dirigé par le joueur : WithoutGPUI. Possibilité de changer le matériau courant avec l'autre disponible par pression du bouton X (Joycon droit) : cela est rendu possible grâce à la méthode Resources.Load, qui permet de récupérer le matériau souhaité se trouvant sous le dossier Resources.
    
    - Sur appui de la touche A (Joycon droit), un clone de l'objet joueur est généré : tous les composants de l'objet original sont conservés (comprenant le matériau sélectionné), excepté le composant script original (remplacé par un autre script destiné aux clones) et tous les fils de l'objet contrôlé par l'utilisateur. (Afin d'éviter le spam de la touche de création de clones, un délai pendant lequel il n'est pas possible de créer une nouvelle copie s'active : 0.75 secondes + temps de création de l'objet clone)
    
    A faire : script complet pour les clones (générer 1 à 4 copies du clone autour de celui-ci (Face Haut et/ou Face Droite et/ou Face Gauche et/ou Face Bas) sur X générations, couleur aléatoire selon matériau), affichage de données importantes (nombre de clones, FPS, nombre de générations de clones en partant de l'objet original (paramétrable) ...)
    
    Note : Ce mini-jeu est le dernier à être développé par mes soins avant le rendu du rapport de projet.

---
+++ Assets/Mini-Games/LifeGame/Scripts/MG_Life_Clone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG_Life_Clone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Un nouveau clone est né !");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Mini-Games/LifeGame/Scripts/MG_Life_Cube.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG_Life_Cube : MonoBehaviour {
+    private List<Joycon> joycons;
+    private Joycon jg, jd;
+    private bool bug, copy;
+    private string pathGPUI, pathNotGPUI;
+
+    //Place les joycons connectés dans la variable correspondante selon s'il s'agit du joycon droit ou du gauche.
+    //Retourne vrai si les variables jg et jd ont été instanciées, faux sinon.
+    private bool initJoycons()
+    {
+        bool ok = false;
+        for (int i = 0; i < joycons.Count; i++)
+        {
+            if (joycons[i].isLeft)
+            {
+                jg = joycons[i];
+            }
+            else
+            {
+                jd = joycons[i];
+            }
+        }
+        if ((jd != null) && (jg != null))
+        {
+            ok = true;
+        }
+        return ok;
+    }
+
+    IEnumerator createClone()
+    {
+        copy = true;
+        //Crée une copie de l'objet contrôlé ...
+        GameObject g = GameObject.Instantiate(gameObject);
+        //... sur laquelle on remplace ce script par celui réservé pour les clones ...
+        Destroy(g.GetComponent<MG_Life_Cube>());
+        g.AddComponent<MG_Life_Clone>();
+        //... et en supprimant tous les enfants de l'objet original.
+        foreach (Transform child in g.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        //Ajout d'un petit délai afin d'éviter le spam d'appui de touche de création de clone.
+        yield return new WaitForSeconds(0.75f);
+        copy = false;
+    }
+
+    //Change le matériau de l'objet contrôlé par le joueur. Si le nom du matériau n'est pas reconnu, rien de notable
+    //se passe. A contrario, s'il y a changement, WithGPUI est un matériau avec GPU Instancing ON tandis que 
+    //WithoutGPUI n'a pas cette option activée.
+    private void switchMaterial()
+    {
+        Material mat = gameObject.GetComponent<Renderer>().material;
+        if(mat.name.Contains("WithGPUI"))
+        {
+            mat = Resources.Load(pathNotGPUI, typeof(Material)) as Material;
+        }
+        else
+        {
+            if(mat.name.Contains("WithoutGPUI"))
+            {
+                mat = Resources.Load(pathGPUI, typeof(Material)) as Material;
+            }
+        }
+        gameObject.GetComponent<Renderer>().material = mat;
+    }
+
+    //Produit la translation à une vitesse speed de l'objet contrôlé par le joueur selon l'orientation des sticks
+    //des Joycons toutes (excepté l'axe X du stick gauche, inutile ici) passées en paramètre.
+    private void stickMovements(float leftY, float rightX, float rightY, float speed)
+    {
+        //Stick droit : Haut / Bas / Gauche / Droite
+        if (rightY > 0)
+        {
+            gameObject.transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+        else
+        {
+            if (rightY < 0)
+            {
+                gameObject.transform.Translate(Vector3.down * speed * Time.deltaTime);
+            }
+        }
+        if(rightX > 0)
+        {
+            gameObject.transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            if(rightX < 0)
+            {
+                gameObject.transform.Translate(Vector3.left * speed * Time.deltaTime);
+            }
+        }
+        //Stick gauche : Avant / Arrière
+        if (leftY > 0)
+        {
+            gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        else
+        {
+            if (leftY < 0)
+            {
+                gameObject.transform.Translate(Vector3.back * speed * Time.deltaTime);
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        joycons = JoyconManager.Instance.j;
+        bug = false;
+        if (joycons.Count == 2)
+        {
+            if (initJoycons())
+            {
+                //Chemin vers les matériaux utilisés pour ce mini-jeu. Ces derniers doivent se 
+                //trouver sous le dossier Resources qui se trouve lui-même sous le dossier du projet.
+                pathGPUI = "materials/withgpui";
+                pathNotGPUI = "materials/withoutgpui";
+                Material mat = Resources.Load(pathNotGPUI, typeof(Material)) as Material;
+                gameObject.GetComponent<Renderer>().material = mat;
+                copy = false;
+            }
+            else
+            {
+                Debug.Log("Absence du joycon droit ou gauche.");
+                bug = true;
+            }
+        }
+        else
+        {
+            Debug.Log("Pas assez ou trop de joycons détectés. Un joycon droit et un gauche nécessaires.");
+            bug = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!bug)
+        {
+            float lsy = jg.GetStick()[1], rsx = jd.GetStick()[0], rsy = jd.GetStick()[1];
+            //Bouton X pressé.
+            if (jd.GetButtonDown(Joycon.Button.DPAD_UP))
+            {
+                switchMaterial();
+            }
+            stickMovements(lsy, rsx, rsy, 25f);
+            //Bouton A pressé et possibilité de créer un clone.
+            if (jd.GetButtonDown(Joycon.Button.DPAD_RIGHT) && (!copy))
+            {
+                StartCoroutine("createClone");
+            }
+        }
+	}
+}
