416b489e488202e2aa26da7743af2fd389281b31
goldsamu <36366367+goldsamu@users.noreply.github.com>
2018-05-14 19:34:56 +0200

    Add files via upload

--- Assets/Mini-Games/Chamboule Tout/Scripts/Canon.cs
+++ Assets/Mini-Games/Chamboule Tout/Scripts/Canon.cs
@@ -36,37 +36,72 @@ public class Canon : MonoBehaviour
         {
             stick = j.GetStick();
 
-            if (transform.position.x < limitX && stick[1] < 0)
+            if (j.isLeft)
             {
-                controls[0] = -stick[1] * vitesse;
-            }
-            else if (transform.position.x > -limitX && stick[1] > 0)
-            {
-                controls[0] = -stick[1] * vitesse;
+                if (transform.position.x < limitX && stick[1] < 0)
+                {
+                    controls[0] = -stick[1] * vitesse;
+                }
+                else if (transform.position.x > -limitX && stick[1] > 0)
+                {
+                    controls[0] = -stick[1] * vitesse;
+                }
+                else
+                {
+                    controls[0] = 0;
+                }
+                if (transform.position.y < limitY && stick[0] > 0)
+                {
+                    controls[1] = stick[0] * vitesse;
+                }
+                else if (transform.position.y > 0 && stick[0] < 0)
+                {
+                    controls[1] = stick[0] * vitesse;
+                }
+                else
+                {
+                    controls[1] = 0;
+                }
+                if (j.GetButton(Joycon.Button.DPAD_DOWN) && delay >= beamRate)
+                {
+                    Instantiate(projectile, firePoint.position, firePoint.rotation);
+                    delay = 0;
+                }
             }
             else
             {
-                controls[0] = 0;
+                if (transform.position.x < limitX && stick[1] > 0)
+                {
+                    controls[0] = stick[1] * vitesse;
+                }
+                else if (transform.position.x > -limitX && stick[1] < 0)
+                {
+                    controls[0] = stick[1] * vitesse;
+                }
+                else
+                {
+                    controls[0] = 0;
+                }
+                if (transform.position.y < limitY && stick[0] < 0)
+                {
+                    controls[1] = -stick[0] * vitesse;
+                }
+                else if (transform.position.y > 0 && stick[0] > 0)
+                {
+                    controls[1] = -stick[0] * vitesse;
+                }
+                else
+                {
+                    controls[1] = 0;
+                }
+
+                if (j.GetButton(Joycon.Button.DPAD_UP) && delay >= beamRate)
+                {
+                    Instantiate(projectile, firePoint.position, firePoint.rotation);
+                    delay = 0;
+                }
             }
 
-            if (transform.position.y < limitY && stick[0] > 0)
-            {
-                controls[1] = stick[0] * vitesse;
-            }
-            else if (transform.position.y > 0 && stick[0] < 0)
-            {
-                controls[1] = stick[0] * vitesse;
-            }
-            else
-            {
-                controls[1] = 0;
-            }
-
-            if (j.GetButton(Joycon.Button.DPAD_DOWN) && delay >= beamRate)
-            {
-                Instantiate(projectile, firePoint.position, firePoint.rotation);
-                delay = 0;
-            }
 
             transform.position += new Vector3(controls[0], controls[1], 0);
         }
